2606940387753ca4e09511e5406ba916e074245b
sandeep <sandeep@vayu.com.au>
2017-04-15 11:01:47 +0545

    add hash_input to generate md5 hash for a given string and return a list of numbers

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,11 +1,19 @@
 defmodule Identicon do
+  @moduledoc """
+    Generate and save Identicon image for a given string input
+  """
+
   def main(input_term) do
     input_term
     |> hash_input
   end
 
+  @doc """
+    Generates md5 hash of the `input_term` (String) passed
+  """
   def hash_input(input_term) do
-
+    :crypto.hash(:md5, input_term)
+    |> :binary.bin_to_list
   end
 
 end
