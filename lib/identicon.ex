7d869687f3c4498f7e22990d60238c455ef242e3
sandeep <sandeep@vayu.com.au>
2017-04-15 11:38:31 +0545

    add pick_color with some bit of elixir patten matching gotcha

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -6,6 +6,21 @@ defmodule Identicon do
   def main(input_term) do
     input_term
     |> hash_input
+    |> pick_color
+  end
+
+  @doc """
+    return [r, g, b] values from image (Identicon.Image struct) passed into it
+  """
+  def pick_color(image) do
+    # use pattern matching to extract RGB values
+    %Identicon.Image{hex: hex_list} = image
+    # just interested in first 3 values, toss rest of the stuff into _tail
+    [r, g, b | _tail] = hex_list
+    # can be rewritten
+    # %Identicon.Image{hex: [r, g, b | _tail]} = image
+
+    [r, g, b]
   end
 
   @doc """
