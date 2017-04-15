25be6f986243ff1abd518060392ed94d5c3937ba
sandeep <sandeep@vayu.com.au>
2017-04-15 11:24:02 +0545

    intro to struct Identicon.Image as single DS to store application data: hex,color, etc

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -10,10 +10,14 @@ defmodule Identicon do
 
   @doc """
     Generates md5 hash of the `input_term` (String) passed
+    Then stores it in a struct Identicon.Image property `hex:`
   """
   def hash_input(input_term) do
-    :crypto.hash(:md5, input_term)
+    hex = :crypto.hash(:md5, input_term)
     |> :binary.bin_to_list
+
+    # store it in struct
+    %Identicon.Image{hex: hex}
   end
 
 end
